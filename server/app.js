6e4ca33c42dc7a162b88841cb6f637af0144081f
= <jishnusundar423@gmail.com>
2017-02-19 23:12:53 -0500

    Installed all npm dependencies

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
+//module inclusions / requirements / dependencies
 let express = require('express');
 let path = require('path'); // part of node.js core
 let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+
+//modules for authentication
+let session = require("express-session");
+let passport = require("passport");
+let passportlocal = require("passport-local");
+let Localstrategy = passportlocal.Strategy;
+let flash = require("connect-flash"); //display errors/login messages
+
 
 // import "mongoose"
 let mongoose = require('mongoose');
